5f0f5ca1fae90dd15dd0ba02b5225a8d09082046
JaekyungLee <jklee78e@gmail.com>
2022-02-10 08:58:09 +0900

    jupyter nb

---
+++ urls.py
@@ -0,0 +1,8 @@
+# /web1/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
